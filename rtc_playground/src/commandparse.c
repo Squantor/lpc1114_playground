2e02ba173f5ad8428b6fa69c6fbe54e0a2523a34
Bart Bilos <anton.bilos@gmail.com>
2016-10-16 16:33:36 +0200

    * added start of first few commands

--- rtc_playground/src/commandparse.c
+++ rtc_playground/src/commandparse.c
@@ -10,17 +10,29 @@ void commandread(char *s, int size)
 	switch(s[1])
 	{
 	case 'r':	// read out rtc date and time, format YYMMDD HHMMSS
-		size = size;
-		uint8_t rtc_data[6];
-		// read out first register
-		Chip_I2C_MasterCmdRead(I2C0, DS3231_SLAVE_ADDR, 0x00, rtc_data, sizeof(rtc_data));
-		print_hex_u8(rtc_data[6]);
-		print_hex_u8(rtc_data[5] & 0x7F);
-		print_hex_u8(rtc_data[4]);
-		Chip_UART_SendRB(LPC_USART, &txring, str_space, sizeof(str_space));
-		print_hex_u8(rtc_data[2]);
-		print_hex_u8(rtc_data[1]);
-		print_hex_u8(rtc_data[0]);
+		{
+			uint8_t rtc_data[6];
+			// read out first register
+			Chip_I2C_MasterCmdRead(I2C0, DS3231_SLAVE_ADDR, 0x00, rtc_data, sizeof(rtc_data));
+			print_hex_u8(rtc_data[6]);
+			print_hex_u8(rtc_data[5] & 0x7F);
+			print_hex_u8(rtc_data[4]);
+			Chip_UART_SendRB(LPC_USART, &txring, str_space, sizeof(str_space));
+			print_hex_u8(rtc_data[2]);
+			print_hex_u8(rtc_data[1]);
+			print_hex_u8(rtc_data[0]);
+		}
+		break;
+	case 't':
+		// read out temperature register
+		{
+			uint8_t rtc_data[3];
+			Chip_I2C_MasterCmdRead(I2C0, DS3231_SLAVE_ADDR, 0x00, rtc_data, sizeof(rtc_data));
+
+		}
+		break;
+	case 'c':
+		// read out calibration register
 		break;
 	}
 }
